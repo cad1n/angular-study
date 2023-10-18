943478221744396dc6fa5f4fedc35853fc1e7043
cad1n <caioaugusto4520@gmail.com>
2023-10-17 21:49:53 -0300

    add servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,10 +2,18 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-servers',
-    templateUrl: './servers.component.html'
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-    constructor(){}
+
+    allowNewServer = false;
+
+    constructor(){
+        setTimeout(() =>{
+            this.allowNewServer = true;
+        }, 2000)
+    }
 
     ngOnInit(): void {
         
