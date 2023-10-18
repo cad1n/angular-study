c5036a407a50947dd827d7879084dada781d471d
cad1n <caioaugusto4520@gmail.com>
2023-10-17 23:57:49 -0300

    add event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,19 +3,24 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: 'app-servers',
     templateUrl: './servers.component.html',
-    styleUrls: ['./servers.component.css']
+    styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
 
     allowNewServer = false;
+    serverCreationStatus = 'No server was created!'
 
-    constructor(){
-        setTimeout(() =>{
+    constructor() {
+        setTimeout(() => {
             this.allowNewServer = true;
         }, 2000)
     }
 
     ngOnInit(): void {
-        
+
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = 'Server was created'
     }
 }
